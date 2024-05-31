72d1b120300bc41e42ba9dfe0ab23d2e779a9ad7
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-05-30 22:32:03 -0300

    Adiciona o pulo do personagem.
    Método Jump no script Player.cs. Para fazer o personagem pular.
    Método OnCollisionEnter2D no script Player.cs. Impedir o pulo duplo.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rbPlayer;
     public float speed;
+    private SpriteRenderer sr;
+    public float jumpForge;
+    public bool inFloor = true;
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
     }
     void Update()
     {
-        MovePlayer();
+        Jump();
     }
     void MovePlayer()
     {
         float horizontalMoviment = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontalMoviment * Time.deltaTime * speed,0,0);
+        transform.position += new Vector3(horizontalMoviment * Time.deltaTime * speed, 0, 0);
+        if (horizontalMoviment > 0)
+        {
+            sr.flipX = false;
+        }
+
+        if (horizontalMoviment < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && inFloor)
+        {
+            rbPlayer.AddForce(new Vector2(0, jumpForge), ForceMode2D.Impulse);
+            inFloor = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject)
+        {
+            inFloor = true;
+        }
     }
 }
