e44ee9d49d3162acba23661fd25eb964af096024
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-06-05 23:11:38 -0300

    Cria o inimigo.
    Adiciona os sprites.
    Faz a animação do inimigo andando.
    Script para o inimigo se movimentar sem sair do espaço devido.

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public bool ground = true;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public bool faceRight = true;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        ground = Physics2D.Linecast(groundCheck.position, transform.position, groundLayer);
+
+        if (ground == false)
+        {
+            speed *= -1;
+        }
+
+        if (speed > 0  && faceRight == false)
+        {
+            Flip();
+        }
+        else if (speed <0 && faceRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        faceRight = !faceRight;
+        Vector3 Scale = transform.localScale;
+        Scale.x *= -1;
+        transform.localScale = Scale;
+    }
+}
