5041fb174a69003bb6f63e7bb8e0870f022933c2
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-06-05 21:01:41 -0300

    Expandir o cenário
    Adiciona mais obstáculos no mapa.
    A câmera acompanha o personagem.
    
    Please enter the commit message for your changes. Lines starting

---
+++ Assets/Script/Camera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = player.position + new Vector3(0, 0, -10);
+        newPosition.y = 0.1f;
+        transform.position = newPosition;
+    }
+}
