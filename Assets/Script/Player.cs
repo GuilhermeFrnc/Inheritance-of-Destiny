cf3834e48740a20e3aecc5b9d7dca66f72cfa466
GuilhermeFrnc <133995611+GuilhermeFrnc@users.noreply.github.com>
2024-06-10 18:33:14 -0300

    Cria o poder do personagem.
    Adiciona sprites do poder.
    Cria um prefab para o poder.
    Adiciona o spawn do poder dos dois lados do personagem.
    Cria um script para fazer o poder sumir depois de um tempo.
    Adiciona métodos no script do personagem para fazer o poder spawnar
    corretamente.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,6 +11,10 @@ public class Player : MonoBehaviour
     public float jumpForge;
     public bool inFloor = true;
     public bool doubleJump;
+    public Transform powerSpawnPointLeft;
+    public Transform powerSpawnPointRight;
+    public GameObject powerPrefab;
+    public float powerSpeed;
     void Start()
     {
         playerAnim = GetComponent<Animator>();
@@ -25,6 +29,37 @@ public class Player : MonoBehaviour
     void Update()
     {
         Jump();
+        Power();
+    }
+
+    void Power()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerAnim.SetTrigger("Player_power");
+
+            Transform selectedSpawnPoint = sr.flipX ? powerSpawnPointLeft : powerSpawnPointRight;
+            var power = Instantiate(powerPrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+            Vector2 direction = sr.flipX ? -selectedSpawnPoint.right : selectedSpawnPoint.right;
+            power.GetComponent<Rigidbody2D>().velocity = direction * powerSpeed;
+
+            Collider2D powerCollider = power.GetComponent<Collider2D>();
+            Collider2D characterCollider = GetComponent<Collider2D>();
+            if (powerCollider != null && characterCollider != null)
+            {
+                Physics2D.IgnoreCollision(characterCollider, powerCollider, true);
+                StartCoroutine(ReenableCollision(characterCollider, powerCollider));
+            }
+        }
+    }
+
+    private IEnumerator ReenableCollision(Collider2D characterCollider, Collider2D powerCollider)
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (characterCollider != null && powerCollider != null)
+        {
+            Physics2D.IgnoreCollision(characterCollider, powerCollider, false);
+        }
     }
     void MovePlayer()
     {
---
+++ Assets/Script/Power.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
